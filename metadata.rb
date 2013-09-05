59fdecccd4743e004301da56b24856d31aead9ce
Tim Flechtner <tflecht@gmail.com>
2013-09-05 12:00:44 -0700

    starting point

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name             'moinmoin'
 maintainer       'Tim Flechtner'
 maintainer_email 'tflecht@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures moinmoin'
+description      'Installs/Configures moinmoin running as a standalone server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
-depends "artifact", "~> 0.10.7"
+depends "artifact", "~> 1.10.0"
